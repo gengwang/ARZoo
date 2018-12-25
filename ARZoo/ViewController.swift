1c35e99ed0a51a9ef08535fa55599abe85168a32
Geng Wang <gwangdesign@yahoo.com>
2018-12-25 09:10:15 -0600

    Add torque.

--- ARZoo/ViewController.swift
+++ ARZoo/ViewController.swift
@@ -54,19 +54,13 @@ class ViewController: UIViewController, ARSCNViewDelegate, SCNPhysicsContactDele
         ]
         
         // Add bottom plane for physics. All boxes fall to this plane will be removed.
-        
-        // For our physics interactions, we place a large node a couple of meters below the world
-        // origin, after an explosion, if the geometry we added has fallen onto this surface which
-        // is place way below all of the surfaces we would have detected via ARKit then we consider
-        // this geometry to have fallen out of the world and remove it
-        
         let bottomPlane = SCNBox(width: 1000.0, height: 0.5, length: 1000.0, chamferRadius: 0.0)
         let bottomPlaneMaterial = SCNMaterial.init()
         bottomPlaneMaterial.diffuse.contents = UIColor.init(red: 0.0, green: 1.0, blue: 0.0, alpha: 0.0)
         bottomPlane.materials = [bottomPlaneMaterial]
         let bottomNode = SCNNode.init(geometry: bottomPlane)
         bottomNode.position = SCNVector3(0.0, -10.0, 0.0)
-        bottomNode.physicsBody = SCNPhysicsBody.init(type: SCNPhysicsBodyType.kinematic, shape: nil)
+        bottomNode.physicsBody = SCNPhysicsBody.init(type: SCNPhysicsBodyType.static, shape: nil)
         bottomNode.physicsBody?.categoryBitMask = CollisionTypes.bottom.rawValue
         bottomNode.physicsBody?.contactTestBitMask = CollisionTypes.cube.rawValue
         sceneView.scene.rootNode.addChildNode(bottomNode)
@@ -110,10 +104,6 @@ class ViewController: UIViewController, ARSCNViewDelegate, SCNPhysicsContactDele
         plane.materials.first?.diffuse.contents = UIColor.init(red: 40/255, green: 160/255, blue: 240/255, alpha: 0.2)
         
         let planeNode = SCNNode(geometry: plane)
-        let planeShape = SCNPhysicsShape.init(geometry: SCNPlane.init(width: 1000, height: 1000), options: nil)
-        planeNode.physicsBody = SCNPhysicsBody.init(type: SCNPhysicsBodyType.kinematic, shape: planeShape)
-        planeNode.physicsBody?.categoryBitMask = CollisionTypes.cube.rawValue
-        planeNode.physicsBody?.contactTestBitMask = CollisionTypes.cube.rawValue
         
         let x = CGFloat(planeAnchor.center.x)
         let y = CGFloat(planeAnchor.center.y)
@@ -135,22 +125,48 @@ class ViewController: UIViewController, ARSCNViewDelegate, SCNPhysicsContactDele
         plane.width = width
         plane.height = height
         
- 
         let x = CGFloat(planeAnchor.center.x)
-        let y = CGFloat(planeAnchor.center.y)
+        let y = CGFloat(0)//CGFloat(planeAnchor.center.y)
         let z = CGFloat(planeAnchor.center.z)
         planeNode.position = SCNVector3(x, y, z)
+        
+        let planeShape = SCNPhysicsShape.init(geometry: plane, options: nil)
+        planeNode.physicsBody = SCNPhysicsBody.init(type: SCNPhysicsBodyType.kinematic, shape: planeShape)
+        planeNode.physicsBody?.categoryBitMask = CollisionTypes.cube.rawValue
+        planeNode.physicsBody?.contactTestBitMask = CollisionTypes.cube.rawValue
     }
     // Override to create and configure nodes for anchors added to the view's session.
     //    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
     //
     //    }
     //
-    @objc func addDiceToSceneView(withGestureRecognizer recognizer: UIGestureRecognizer) {
-        let tapLocation = recognizer.location(in: sceneView)
-        let hitTestResults = sceneView.hitTest(tapLocation, types: .existingPlaneUsingExtent)
+    // Add a dice to the scene
+    @objc func handleSingleTap(withGestureRecognizer recognizer: UIGestureRecognizer) {
+        let location = recognizer.location(in: sceneView)
+        guard let result = sceneView.hitTest(location, options: nil).first else { return }
+        if result.node.name == "Dice" {
+            print(" -- roll dice")
+            rollDice(at: location)
+        } else {
+            addDice(at: location)
+        }
+    }
+    
+    @objc func handleLongPress(withGestureRecognizer recognizer: UILongPressGestureRecognizer) {
+//        liftDice(at: recognizer.location(in: sceneView))
+//        rollDice(at: recognizer.location(in: sceneView))
+    }
+     @objc func handleDoubleTap(withGestureRecognizer recognizer: UILongPressGestureRecognizer) {
+//        rollDice(at: recognizer.location(in: sceneView))
+    }
+    
+    private func addDice(at location: CGPoint) {
+        if let result = sceneView.hitTest(location, options: nil).first, result.node.name == "Dice" { return }
+        
+        let hitTestResults = sceneView.hitTest(location, types: .existingPlaneUsingExtent)
         
         guard let hitTestResult = hitTestResults.first else { return }
+        
         let translation = hitTestResult.worldTransform.position()
         let insertionYOffset: Float = 0.3
         let x = translation.x
@@ -162,7 +178,7 @@ class ViewController: UIViewController, ARSCNViewDelegate, SCNPhysicsContactDele
             let keyLightNode = diceScene.rootNode.childNode(withName: "Sun", recursively: false),
             let fillLightNode = diceScene.rootNode.childNode(withName: "Point", recursively: false)
             else { return }
-
+        
         // Note: Physics body is configured in the scn file.
         // Set the physics shape scale to match that of the transform scale of the geometry
         // mass: 2.0
@@ -174,37 +190,62 @@ class ViewController: UIViewController, ARSCNViewDelegate, SCNPhysicsContactDele
         sceneView.scene.rootNode.addChildNode(keyLightNode)
         sceneView.scene.rootNode.addChildNode(fillLightNode)
     }
-    
-    @objc func handleLongPress(withGestureRecognizer recognizer: UILongPressGestureRecognizer) {
-        let viewTouchLocation:CGPoint = recognizer.location(in: sceneView)
-        guard let result = sceneView.hitTest(viewTouchLocation, options: nil).first else {
+    private func liftDice(at location: CGPoint) {
+        guard let result = sceneView.hitTest(location, options: nil).first else {
             return
         }
         if result.node.name == "Dice" {
             
-//            CASpringAnimation.init(keyPath: <#T##String?#>)
+            //            CASpringAnimation.init(keyPath: <#T##String?#>)
             // Test quick animation
             SCNTransaction.begin()
             SCNTransaction.animationDuration = 0.333
             SCNTransaction.animationTimingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.easeInEaseOut)
-            result.node.position.y = 2
+            result.node.position.y = 0.025 * 1 // n units up
             SCNTransaction.commit()
             // test
-//            let diceGeometryNode = result.node.childNode(withName: "Dice", recursively: false)
-//            print("--dice?? \(diceGeometryNode)")
+            //            let diceGeometryNode = result.node.childNode(withName: "Dice", recursively: false)
+            //            print("--dice?? \(diceGeometryNode)")
             for node in result.node.childNodes {
                 print("--node: \(node)")
             }
         }
     }
+    private func rollDice(at location: CGPoint) {
+        guard let result = sceneView.hitTest(location, options: nil).first else {
+            return
+        }
+        let node = result.node
+        if node.name != "Dice" { return }
+        
+        let randomX = Float.random(in: -0.02...0.02) //-0.2...0.2
+        let randomY = Float.random(in: -0.01...0.01) //1.0...18
+        let randomZ = Float.random(in: 1.0...1.2) //1.0...18
+        let randomW = Float.random(in: 0.08...0.12)
+        print("  impulse!")
+        // -y is the direction that the camera is looking at
+        //        let force = SCNVector3(x: randomX, y: randomY, z: randomZ)
+        //        let position = node.position
+        //        node.physicsBody?.applyForce(force, at: position, asImpulse: true)
+        
+        //        let torque = SCNVector4(randomX, randomY, randomZ, randomW)
+        let torque = SCNVector4(randomX, randomY, randomZ, randomW)
+        print("force: \(torque)")
+        node.physicsBody?.applyTorque(torque, asImpulse: true)
+    }
     
     func setupGestureRecognizers() {
-        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(ViewController.addDiceToSceneView(withGestureRecognizer:)))
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(ViewController.handleSingleTap(withGestureRecognizer:)))
+        tapGestureRecognizer.numberOfTapsRequired = 1
         sceneView.addGestureRecognizer(tapGestureRecognizer)
         
         let longPressRecognizer = UILongPressGestureRecognizer(target: self, action: #selector(ViewController.handleLongPress(withGestureRecognizer:)))
         longPressRecognizer.minimumPressDuration = 0.5
         sceneView.addGestureRecognizer(longPressRecognizer)
+        
+        let doubleTapRecognizer = UITapGestureRecognizer(target: self, action: #selector(ViewController.handleDoubleTap(withGestureRecognizer:)))
+        doubleTapRecognizer.numberOfTapsRequired = 2
+        sceneView.addGestureRecognizer(doubleTapRecognizer)
     }
     
     func session(_ session: ARSession, didFailWithError error: Error) {
